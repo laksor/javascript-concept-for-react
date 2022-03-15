2ebefbc7a0e51de8a9cb76c5797f752c5c910ba2
laskor <al.moksud@gmail.com>
2022-03-15 18:18:08 -0400

    variable

--- fundamentals.js
+++ fundamentals.js
@@ -1,3 +1,11 @@
 // 6 fundamentals
 
-// 1.
+// 1. declare a variable using let and const
+
+const fatherName = 'adam';
+let season = 'rainy';
+season = 'winter';
+
+// 2.condition
+// 6 basic condition ( > , < , === , !== , <= , >=)
+// multiple conditional operator : && (AND), ||(OR)
