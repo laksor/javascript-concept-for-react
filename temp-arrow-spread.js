b4cf5250fc7ff1a032b9348a9ef41626c983a593
laskor <al.moksud@gmail.com>
2022-03-16 00:27:51 -0400

    arrow

--- temp-arrow-spread.js
+++ temp-arrow-spread.js
@@ -8,8 +8,19 @@ const student = {
 }
 
 // template string back tik : ``
-const about = `my name is ${student.name} age of ${student.age} hs number ${number[2]} also like movies ${student.movies[0]}`;
+const about = `my name is ${student.name} age of ${student.age} has number ${arr[2]} also like movies ${student.movies[0]}`;
 console.log(about);
 
 // arrow function 
+const getF = () => 55;
+const add = num => num + 5;
+const isEven = x => x % 2 == 0;
+const addThree = (num1, num2, num3) => num1 + num2 + num3;
+// multi line
+const doMath = (num1, num2) => {
+    const sum = num1 + num2;
+    return sum;
+}
+console.log(getF());
 
+// spread operator
