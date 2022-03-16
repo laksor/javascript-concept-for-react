4e220fa652ca94a94c1810ba585f06f4d84cd319
laskor <al.moksud@gmail.com>
2022-03-16 00:22:46 -0400

    template string

---
+++ temp-arrow-spread.js
@@ -0,0 +1,15 @@
+// template string, arrow function, spread operator
+
+const arr = [3, 4, 5, 6, 7];
+const student = {
+    name: 'sakib',
+    age: 45,
+    movies: ['king', 'sallu'],
+}
+
+// template string back tik : ``
+const about = `my name is ${student.name} age of ${student.age} hs number ${number[2]} also like movies ${student.movies[0]}`;
+console.log(about);
+
+// arrow function 
+
