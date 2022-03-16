670d7d2dd302cca2817a884794dbae36c6225ecb
laskor <al.moksud@gmail.com>
2022-03-16 02:42:04 -0400

    str to num

---
+++ ternary.js
@@ -0,0 +1,17 @@
+// truthy, false Ternary operator, shortcut and or
+
+const money = 120;
+const myVar = 120;
+
+// ternary operator (if else shortcut)
+
+let food = money > 100 ? 'biryani' : 'cha';
+console.log(food);
+
+let food1 = (money > 100 && myVar > 100) ? 'water' : 'pani';
+console.log(food1);
+
+// string to number
+const input = '500';
+const inputNum = +input;
+console.log(inputNum);
