693aadaf0dcd9d5f2c3485e7f3524418d3a350c2
laskor <al.moksud@gmail.com>
2022-03-16 01:59:59 -0400

    object

--- destructuring.js
+++ destructuring.js
@@ -1,10 +1,32 @@
 // Array and object Destructuring, optional chaining
 
-const products = [
-    {name: 'laptop', price: 3000, brand: 'lenovo', color: 'blue'},
-    {name: 'watch', price: 4000, brand: 'samsung', color: 'red'},
-    {name: 'phone', price: 35000, brand: 'apple', color: 'silver'},
-    {name: 'sunGlass', price: 300, brand: 'rivon', color: 'gold'},
-    {name: 'camera', price: 30000, brand: 'canon', color: 'yellow'}
-];
+// const products = [
+//     {name: 'laptop', price: 3000, brand: 'lenovo', color: 'blue'},
+//     {name: 'watch', price: 4000, brand: 'samsung', color: 'red'},
+//     {name: 'phone', price: 35000, brand: 'apple', color: 'silver'},
+//     {name: 'sunGlass', price: 300, brand: 'rivon', color: 'gold'},
+//     {name: 'camera', price: 30000, brand: 'canon', color: 'yellow'}
+// ];
 
+const numbers = [33, 44];
+
+// old method:
+
+// const x = numbers[0];
+// const y = numbers[1];
+
+//  Destructuring
+const [firstN, secondN] = numbers;
+const [x, y] = [33, 44];
+console.log(x, y);
+
+function box (num1 , num2){
+    const nums = [num1, num2];
+    return nums;
+}
+const [first, second] = box(90, 44);
+// console.log(box(44, 44));
+
+// object  Destructuring
+
+const {name, age} = { name: 'alu', age: 66 };
