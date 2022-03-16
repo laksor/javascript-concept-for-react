e8a6c9509a5e918f0e925d35b4219a682ee69a07
laskor <al.moksud@gmail.com>
2022-03-16 01:50:24 -0400

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,10 @@
+// Array and object Destructuring, optional chaining
+
+const products = [
+    {name: 'laptop', price: 3000, brand: 'lenovo', color: 'blue'},
+    {name: 'watch', price: 4000, brand: 'samsung', color: 'red'},
+    {name: 'phone', price: 35000, brand: 'apple', color: 'silver'},
+    {name: 'sunGlass', price: 300, brand: 'rivon', color: 'gold'},
+    {name: 'camera', price: 30000, brand: 'canon', color: 'yellow'}
+];
+
