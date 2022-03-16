fd32dda2ec6be41b4f588405c1bc3d58f2b05230
laskor <al.moksud@gmail.com>
2022-03-16 02:33:15 -0400

    remove

--- json.js
+++ json.js
@@ -32,3 +32,20 @@ console.log(value);
 
 // for of using on array like object
 // for in using on object
+
+const products = [
+    {name: 'laptop', price: 3000, brand: 'lenovo', color: 'blue'},
+    {name: 'watch', price: 4000, brand: 'samsung', color: 'red'},
+    {name: 'phone', price: 35000, brand: 'apple', color: 'silver'},
+    {name: 'sunGlass', price: 300, brand: 'rivon', color: 'gold'},
+    {name: 'camera', price: 30000, brand: 'canon', color: 'yellow'}
+];
+
+const newProduct = { name: 'webcam', price: 5000, brand: 'lal'};
+
+// copy product array and then add new using spread operator
+const newProducts = [...products, newProduct];
+
+// create a new array without one specidic item
+// remove a phone and create new array
+const remaining = products.filter(product => product.name !== 'phone');
