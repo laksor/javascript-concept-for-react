12284a7ad030adcc84dd12fb0b8533fcbcfb1064
laskor <al.moksud@gmail.com>
2022-03-16 02:45:18 -0400

    truthy

--- ternary.js
+++ ternary.js
@@ -11,7 +11,13 @@ console.log(food);
 let food1 = (money > 100 && myVar > 100) ? 'water' : 'pani';
 console.log(food1);
 
-// string to number
+// string to number (only using + sign)
 const input = '500';
 const inputNum = +input;
 console.log(inputNum);
+
+// truthy & falsy
+const isActive = true;
+const showUser = () => console.log('display user');
+const showNumber = () => console.log('Number');
+isActive ? showUser() : showNumber();
